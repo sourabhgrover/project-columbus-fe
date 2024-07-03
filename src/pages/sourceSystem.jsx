eadbb776413f8319635c34436262afb515f10748
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2024-07-03 16:49:31 +0200

    Source system

--- src/pages/sourceSystem.jsx
+++ src/pages/sourceSystem.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import SourceSystems from '../components/sourceSystems'; // Adjust the path as necessary
+import React, { useEffect } from 'react'
+ import SourceSystems from "../components/sourceSystems"; // Adjust the path as necessary
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSchemas, setSelectedSchemaId } from "../rtk/schemas";
 
 function Home() {
-    return (
-        <div className="p-4">
-            <SourceSystems />
-        </div>
-    );
+  const dispatch = useDispatch();
+  const schemas = useSelector((state) => state.schemas);
+  console.log(schemas);
+  useEffect(() => {
+    dispatch(fetchSchemas());
+  }, []);
+  return (
+    <div className="p-4">
+      <SourceSystems />
+    </div>
+  );
 }
 
 export default Home;
