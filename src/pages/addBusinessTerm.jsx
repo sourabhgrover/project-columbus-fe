aadecad82542eebeb6ea513a2b06a909825b0b28
sourabhgrover <sourabhgrover29@gmail.com>
2024-07-06 15:52:41 +0530

    Add Businnes Term

--- src/pages/addBusinessTerm.jsx
+++ src/pages/addBusinessTerm.jsx
@@ -1,44 +1,66 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
-import TagInput from "../components/tagInput"
-import { createBusinessTerm } from '../rtk/addBusinessTerm';
-import { useDispatch, useSelector } from 'react-redux';
-import Toast from '../components/Toast';
-import { useNavigate } from 'react-router-dom';
+import TagInput from "../components/tagInput";
+import { createBusinessTerm } from "../rtk/addBusinessTerm";
+import { useDispatch, useSelector } from "react-redux";
+import Toast from "../components/Toast";
+import { useNavigate } from "react-router-dom";
 
 export default function AddBusinessTerm() {
-  const { register, handleSubmit,reset, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { success } = useSelector((state) => state.manageBusinessTerm);
   const [tags, setTags] = useState([]);
 
-  const onSubmit = data => {console.log(data)
+  useEffect(() => {
+    setValue("tags", tags);
+  }, [tags]);
+
+  const onSubmit = (data) => {
     dispatch(createBusinessTerm(data));
   };
 
   useEffect(() => {
     if (success) {
       reset();
-      navigate('/business-glossary');
+      // navigate('/business-glossary');
     }
-  },[success])
+  }, [success]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Add a new term in the Business Glossary</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Add a new term in the Business Glossary
+          </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            A Business Term in the context of a business glossary refers to a specific word or phrase that has a particular meaning within the context of a business or organization. Business terms are essential for ensuring clear and consistent communication across an organization, as they define and standardize the language used to describe various business concepts, processes, roles, data elements, and metrics.
-            Once you add a new business term it will be sent for approval to the Domain Owner and once approved it will be visible to all the users of the Data Catalog
+            A Business Term in the context of a business glossary refers to a
+            specific word or phrase that has a particular meaning within the
+            context of a business or organization. Business terms are essential
+            for ensuring clear and consistent communication across an
+            organization, as they define and standardize the language used to
+            describe various business concepts, processes, roles, data elements,
+            and metrics. Once you add a new business term it will be sent for
+            approval to the Domain Owner and once approved it will be visible to
+            all the users of the Data Catalog
           </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
-              <label htmlFor="Business Term" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="Business Term"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Business Term Name
               </label>
               <div className="mt-2">
@@ -46,35 +68,38 @@ export default function AddBusinessTerm() {
                   <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
                   <input
                     type="text"
-                    // name="username"
-                    // id="username"
-                    // autoComplete="username"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="Milk Valorisation"
-                    {...register('name')}
+                    {...register("name")}
                   />
                 </div>
               </div>
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Defintion
               </label>
               <div className="mt-2">
                 <textarea
-                  // id="about"
-                  // name="about"
                   rows={3}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                  {...register('definition')}
+                  defaultValue={""}
+                  {...register("definition")}
                 />
               </div>
-              <p className="mt-3 text-sm leading-6 text-gray-600">Provide an accurate and relevant defintion for the business term</p>
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                Provide an accurate and relevant defintion for the business term
+              </p>
             </div>
             <div className="sm:col-span-4">
-              <label htmlFor="Business Term" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="Business Term"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Parent Business Term
               </label>
               <div className="mt-2">
@@ -82,42 +107,51 @@ export default function AddBusinessTerm() {
                   <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
                   <input
                     type="text"
-                    // name="username"
-                    // id="username"
-                    // autoComplete="username"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="Milk"
-                    {...register('parentTerm')}
+                    {...register("parentTerm")}
                   />
                 </div>
               </div>
             </div>
 
             <div className="col-span-full mt-4">
-              <label htmlFor="tags" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="tags"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Tags
               </label>
               <div className="mt-2">
                 <TagInput
+                  name="tags"
                   tags={tags}
                   setTags={setTags}
-                  {...register('tags', { valueAsArray: true })}
+                  {...register("tags", { valueAsArray: true })}
                 />
               </div>
               <p className="mt-3 text-sm leading-6 text-gray-600">
-                Add relevant tags to the business term for better categorization.
+                Add relevant tags to the business term for better
+                categorization.
               </p>
             </div>
           </div>
         </div>
 
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Business Term Owner</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Provide further details for the Business Term</p>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Business Term Owner
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            Provide further details for the Business Term
+          </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 First name
               </label>
               <div className="mt-2">
@@ -127,52 +161,54 @@ export default function AddBusinessTerm() {
                   // id="first-name"
                   // autoComplete="given-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  {...register('owner.firstName')}
+                  {...register("owner.firstName")}
                 />
               </div>
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Last name
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  // name="last-name"
-                  // id="last-name"
-                  // autoComplete="family-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  {...register('owner.lastName')}
+                  {...register("owner.lastName")}
                 />
               </div>
             </div>
 
             <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Email address
               </label>
               <div className="mt-2">
                 <input
-                  // id="email"
-                  // name="email"
                   type="email"
                   autoComplete="email"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  {...register('owner.email')}
+                  {...register("owner.email")}
                 />
               </div>
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Domain
               </label>
               <div className="mt-2">
                 <select
-                  // id="domain"
-                  {...register('owner.domain')}
-                  // name="domain"
+                  {...register("owner.domain")}
                   autoComplete="domain-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
@@ -184,7 +220,6 @@ export default function AddBusinessTerm() {
                   <option>R&D</option>
                   <option>Commerce - B2C</option>
                   <option>Commerce - B2B</option>
-
                 </select>
               </div>
             </div>
@@ -192,64 +227,79 @@ export default function AddBusinessTerm() {
         </div>
 
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Notifications
+          </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what else you want to hear about.
+            We'll always let you know about important changes, but you pick what
+            else you want to hear about.
           </p>
 
           <div className="mt-10 space-y-10">
             <fieldset>
-              <legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                By Email
+              </legend>
               <div className="mt-6 space-y-6">
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
                     <input
-                      // id="comments"
-                      // name="comments"
                       type="checkbox"
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                      {...register('notifications.comments')}
+                      {...register("notifications.comments")}
                     />
                   </div>
                   <div className="text-sm leading-6">
-                    <label htmlFor="comments" className="font-medium text-gray-900">
+                    <label
+                      htmlFor="comments"
+                      className="font-medium text-gray-900"
+                    >
                       Comments
                     </label>
-                    <p className="text-gray-500">Get notified when someones posts a comment on a Business Term.</p>
+                    <p className="text-gray-500">
+                      Get notified when someones posts a comment on a Business
+                      Term.
+                    </p>
                   </div>
                 </div>
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
                     <input
-                      // id="candidates"
-                      // name="candidates"
-                      {...register('notifications.candidates')}
+                      {...register("notifications.candidates")}
                       type="checkbox"
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
                   </div>
                   <div className="text-sm leading-6">
-                    <label htmlFor="candidates" className="font-medium text-gray-900">
+                    <label
+                      htmlFor="candidates"
+                      className="font-medium text-gray-900"
+                    >
                       Candidates
                     </label>
-                    <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
+                    <p className="text-gray-500">
+                      Get notified when a candidate applies for a job.
+                    </p>
                   </div>
                 </div>
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
                     <input
-                      // id="offers"
-                      // name="offers"
-                      {...register('notifications.approval')}
+                      {...register("notifications.approval")}
                       type="checkbox"
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
                   </div>
                   <div className="text-sm leading-6">
-                    <label htmlFor="offers" className="font-medium text-gray-900">
+                    <label
+                      htmlFor="offers"
+                      className="font-medium text-gray-900"
+                    >
                       Approval
                     </label>
-                    <p className="text-gray-500">Get notified when a term is accepted or rejected by owner.</p>
+                    <p className="text-gray-500">
+                      Get notified when a term is accepted or rejected by owner.
+                    </p>
                   </div>
                 </div>
               </div>
@@ -259,7 +309,11 @@ export default function AddBusinessTerm() {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" onClick={() => reset()} className="text-sm font-semibold leading-6 text-gray-900">
+        <button
+          type="button"
+          onClick={() => reset()}
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
           Cancel
         </button>
         <button
@@ -270,5 +324,5 @@ export default function AddBusinessTerm() {
         </button>
       </div>
     </form>
-  )
+  );
 }
