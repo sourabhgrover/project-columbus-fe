7f3a52e426ef5155b0d74f3a916e4a59064042c9
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2024-12-23 11:59:56 +0100

    Latest Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import formsPlugin from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,7 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    formsPlugin,
   ],
-}
-
+};
